3ff1febfcaead72927d812818ce1015188d53f2f
Néstor Argüello <nestor.arguello@gmail.com>
2017-11-24 04:45:53 +0000

    Accesing Records

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122172623) do
+ActiveRecord::Schema.define(version: 20171124041825) do
 
   create_table "albums", force: :cascade do |t|
     t.string "name"
     t.integer "release_year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.integer "book_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Album.create(name:	"Sgt.	Pepper's	Lonely	Hearts	Club	Band",	release_year:	
+Album.create(name:	"Sgt. Pepper's Lonely	Hearts	Club	Band",	release_year:	
 1967)
-Album.create(name:	"Pet	Sounds",	release_year:	1966)
+Album.create(name:	"Pet Sounds",	release_year:	1966)
 Album.create(name:	"Revolver",	release_year:	1966)
-Album.create(name:	"Highway	61	Revisited",	release_year:	1965)
+Album.create(name:	"Highway 61 Revisited",	release_year:	1965)
 Album.create(name:	"Rubber	Soul",	release_year:	1965)
 Album.create(name:	"What's	Going	On",	release_year:	1971)
-Album.create(name:	"Exile	on	Main	St.",	release_year:	1972)
+Album.create(name:	"Exile on Main St.",	release_year:	1972)
 Album.create(name:	"London	Calling",	release_year:	1979)
-Album.create(name:	"Blonde	on	Blonde",	release_year:	1966)
-Album.create(name:	"The	Beatles",	release_year:	1968)
+Album.create(name:	"Blonde	on Blonde",	release_year:	1966)
+Album.create(name:	"The Beatles",	release_year:	1968)
